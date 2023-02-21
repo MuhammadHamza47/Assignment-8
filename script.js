2b8801976b6002a9500f9aec8253f901d4c01f5a
Muhammad Hamza <hafizhamza2247@gmail.com>
2023-02-21 15:02:41 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,69 @@
+// Question#1
+let name= "Umair Azmat";
+let admin= name;
+alert(admin);
+// Question#2
+let earth_planet;
+// Question#3
+let current_visitor;
+// Question#4
+let favDrink= "Pepsi";
+// Question#5
+let favNum = 484;
+// Question#6
+function myFavs(favDrink)
+{
+    return favDrink;
+
+}
+console.log(myFavs("Pepsi"))
+alert(myFavs("Shoaib"));
+// object
+const person = {Name:"Umair", Age:"20"};
+console.log(person)
+// Question#8
+let a= 23;
+let b= 53;
+let result= a+b;
+console.log(result)
+// Q9
+let c=84;
+let d=83;
+let divide= c/d;
+console.log(divide)
+// Q10
+let f1= 3;
+let f2= 2;
+let Result= f1+f2;
+final= Result/5;
+console.log(final)
+// Q11
+let F=50;
+let C= (F-32)*5/9;
+console.log(C);
+// Q12
+let e=  parseInt(prompt("Enter first number"));
+let f= parseInt(prompt("Enter second number"));
+let result2= e+f;
+alert(result2)
+// Q13
+let e1=  parseInt(prompt("Enter first number"));
+let f3= parseInt(prompt("Enter second number"));
+let result3= e1+f3;
+console.log(result3);
+// Q14
+let e2=  parseInt(prompt("Enter first number"));
+let f4= parseInt(prompt("Enter second number"));
+let result4= e2*f4;
+alert(result4);
+// Q15
+let e3=  parseInt(prompt("Enter first number"));
+let f5= parseInt(prompt("Enter second number"));
+let result5= e3/f5;
+console.log(result5);
+// Q16
+let e4=  parseInt(prompt("Enter first number"));
+let f6= parseInt(prompt("Enter second number"));
+let result6= e4%f6;
+alert(result6);
+
